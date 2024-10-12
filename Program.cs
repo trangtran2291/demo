57ed9295f21b32fff7a080eac651a27f5c91c221
trangtran2291 <trangtran.31231022291@st.ueh.edu.vn>
2024-10-12 18:05:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}*/
+namespace ConsoleApp1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tran Thao Trang");
+            Console.WriteLine(2005);
+            Console.WriteLine("Femal");
+            Console.WriteLine(19);
+            Console.ReadLine();
+        }
+
+    }
+}
